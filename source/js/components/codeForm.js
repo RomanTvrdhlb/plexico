89bc6b9157516179ea46515e15e033800b1d5e97
roman <romasaab@gmail.com>
2024-10-20 02:48:14 +0300

    add new pages

--- source/js/components/codeForm.js
+++ source/js/components/codeForm.js
@@ -1,5 +1,6 @@
 const parentForms = document.querySelectorAll(".main-form");
 const connectForms =  document.querySelectorAll('.connect-card');
+const copyForms =  document.querySelectorAll('.copy-form');
 
 parentForms &&
   parentForms.forEach(function (form) {
@@ -45,6 +46,33 @@ parentForms &&
         } catch (error) {
           input.value = 'Ошибка';
           input.style.color = '#E34545';
+        }
+      }
+    });
+  });
+
+  copyForms && copyForms.forEach(card => {
+    const input = card.querySelector('.copy-form__input');
+    const button = card.querySelector('.copy-form__btn');
+
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (input) {
+        input.select();
+        try {
+          const copied = document.execCommand('copy');
+          
+          if (copied) {
+            input.value = 'Скопировано';
+            input.style.color = '#00AC45';
+            input.style.textAlign = 'center';
+          } else {
+            throw new Error('Copy failed');
+          }
+        } catch (error) {
+          input.value = 'Ошибка';
+          input.style.color = '#E34545';
+          input.style.textAlign = 'center';
         }
       }
     });
